d9f4083a2cb478460d268ad15b45106b8159f446
Sarabjeet <sarabjeet.singh@imaginea.com>
2018-08-10 10:31:00 +0530

    adding generic flight booking script

---
+++ DCSFlyDubai/Script/TestPath.js
@@ -0,0 +1,8 @@
+﻿var Common = require("Common");
+var ReadDataFromTextFile = require("ReadDataFromTextFile");
+
+function Check(){
+var path = Common.GetGenericFilePath();
+var config = ReadDataFromTextFile.ReadData(path + "configapis.txt");
+var k = 3;
+}
